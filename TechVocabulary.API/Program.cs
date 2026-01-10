dd646cf5cba5aeb7dde29d357a1ce1368638db8d
Rijo-010 <rijo.reji@ust.trumio.ai>
2026-01-10 12:36:32 +0530

    Game Service and Controller COmpleted

--- TechVocabulary.API/Program.cs
+++ TechVocabulary.API/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")
     ));
+builder.Services.AddScoped<IGameService, GameService>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApi();
---
+++ TechVocabulary.API/Services/IGameService.cs
@@ -0,0 +1,6 @@
+public interface IGameService
+{
+    GameQuestionResponse GetNextQuestion(int userId);
+    AnswerResultResponse ValidateAnswer(int userId, AnswerRequest request);
+    GameScoreResponse CalculateScore(int userId);
+}

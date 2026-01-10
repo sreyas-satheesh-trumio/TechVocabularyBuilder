1ad0bfc4e717abb2b7820fe5f9596366f2918ca3
Rijo-010 <rijo.reji@ust.trumio.ai>
2026-01-10 12:50:19 +0530

    DTOs Added

---
+++ TechVocabulary.Contracts/DTOs/AnswerRequest.cs
@@ -0,0 +1,5 @@
+public class AnswerRequest
+{
+    public int TopicId { get; set; }
+    public string SelectedAnswer { get; set; }
+}
---
+++ TechVocabulary.Contracts/DTOs/GameScoreResponse.cs
@@ -0,0 +1,6 @@
+public class GameScoreResponse
+{
+    public int TotalQuestions { get; set; }
+    public int CorrectAnswers { get; set; }
+    public int TotalScore { get; set; }
+}
